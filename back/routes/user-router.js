c4843c85e785c4fce1be56c07c3b3d9bed340d76
Esmeralda R <esmeralda.re@outlook.com>
2017-01-05 15:33:05 -0500

    start of user routes

--- back/routes/user-router.js
+++ back/routes/user-router.js
@@ -7,7 +7,7 @@ const User = require('../../models').User
 
 /////////////////////
 ////FUNCTION/////////
-const getUser = ((request,response) => {
+const getAllUsers = ((request,response) => {
 	User.findAll()
 		.then(data => {
 			response.send(data)
@@ -17,11 +17,62 @@ const getUser = ((request,response) => {
 		})
 })
 
+const makeNewUser = ((request,response) => {
+  console.log(request.body);
+  User.create({
+    username:request.body.username,
+    password:request.body.password,
+    firstName:request.body.firstName,
+    lastName:request.body.lastName,
+    email:request.body.email
+  })
+    .then(data => {
+      response.send(data)
+    })
+    .catch(error => {
+      response.send(error)
+    })
+})
+
+//find user by username
+const getUser = ((request,response) => {
+  User.findAll({
+      where: {username: request.params.username}
+    })
+    .then(data => {
+      response.send(data)
+    })
+    .catch(error => {
+      response.send(error)
+    })
+})
+
+//see who you're following
+// const getUsersYouFollow = ((request,response) => {
+//   User.findAll()
+//     .then(data => {
+//       response.send(data)
+//     })
+//     .catch(error => {
+//       response.send(error)
+//     })
+// })
+
+//search for blogs or people to follow
+
+
 /////////////////////
 //////ROUTE//////////
 /////////////////////
 router.route('/')
-	.get(getUser)
+	.get(getAllUsers)
+  .post(makeNewUser)
+
+router.route('/:username')
+  .get(getUser)
+
+// router.route('/:userId')
+//   .get(getUsersYouFollow)
 
 
 /////////////////////
