42bbf0d7c183db2f1575465389e911a4a44ba8a0
Melanie Williams <melaniep518@gmail.com>
2017-01-06 12:47:45 -0500

    more tests

--- test/user-tests.js
+++ test/user-tests.js
@@ -17,15 +17,46 @@ describe('User tests', () => {
     .catch((err) => console.log('DB Error', err));
   });
 
+// tests GET all users
   it(`'/' should respond with all users`, (done) => {
     supertest(server)
     .get('/api/user')
     .end((err, res) => {
-      console.log('RESPONSE BODY LENGTH:', res.body.length);
-      console.log('RESPONSE BODY:', res.body);
+      // console.log('RESPONSE BODY LENGTH:', res.body.length);
+      // console.log('RESPONSE BODY:', res.body);
       expect(res.body).be.a('array');
       expect(res.body.length).equal(3);
       done();
     })
+  });
+
+// tests GET one user by id
+  it(`'/:username' should return one user`, (done) => {
+    supertest(server)
+    .get('/api/user/user1')
+    .end((err, res) => {
+      console.log(res.body);
+      expect(res.body).be.a('array');
+      expect(res.body.length).equal(1);
+      expect(res.body[0].id).equal(1);
+      done();
+    })
   })
+
+// tests POST a new user
+  it(`'/' should respond with a new user`, (done) => {
+    supertest(server)
+    .post('/api/user')
+    .type('form')
+    .send({username: 'user4', password: 'pass4', firstName: 'first4', lastName: 'last4', email: 'email4@email.com'})
+    .end((err, res) => {
+      console.log(res.body);
+      expect(res.body).exist;
+      expect(res.body).be.a('object');
+      done();
+    })
+  });
+
+
+
 })
