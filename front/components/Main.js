37ca98ec95275369b6dcceed2b91a93e6616a03e
Victor Amos <victoramosjr@gmail.com>
2017-01-10 12:33:51 -0500

    Add CreatePost component

--- front/components/Main.js
+++ front/components/Main.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import SiteNavigation from './navigation/SiteNavigation.js';
+import CreatePost from './create-post/Create-Post.js';
 
 //Build Component
 const Main = React.createClass({
@@ -14,6 +15,7 @@ const Main = React.createClass({
 					</Link>
 				</h1>
 				<SiteNavigation />
+				<CreatePost />
 				{React.cloneElement(this.props.children, this.props)}
 			</div>
 		)
