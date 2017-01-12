66dc94f30a7793c4dbf96578d4b692591e18bd39
Victor Amos <victoramosjr@gmail.com>
2017-01-12 10:31:20 -0500

    Add Individual Post Footer

--- front/components/timeline/IndividualPostFooter.js
+++ front/components/timeline/IndividualPostFooter.js
@@ -1,1 +1,20 @@
-IndividualPostFooter.js
+import React from 'react';
+
+const IndividualPostFooter = React.createClass({
+	render() {
+		return(
+			<div>
+				<div className="footer-container">
+					<div className="footer-left">
+						1,275 notes
+					</div>		
+					<div className="footer-right">
+						Footer Right
+					</div>
+				</div>		
+			</div>
+		)
+	}
+});
+
+export default IndividualPostFooter;
